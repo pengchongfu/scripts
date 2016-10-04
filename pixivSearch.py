6d8424f0e6e8e0e292beb7842a6ab2af90405709
steinsphang <824780925@qq.com>
2016-10-04 23:42:03 +0800

    replace with regular

--- pixivSearch.py
+++ pixivSearch.py
@@ -6,8 +6,7 @@ import threading
 import sys
 import math
 import Queue
-import time
-from bs4 import BeautifulSoup
+import re
 
 class geturl(threading.Thread):
     def __init__(self,name,keyword,number):
@@ -24,15 +23,12 @@ class geturl(threading.Thread):
             lock.release()
             try:
                 html = s.get('http://www.pixiv.net/search.php?word='+self.keyword+'&p='+str(item))
-                soup = BeautifulSoup(html.text,"html.parser")    
-                for li in soup.find_all('li',class_='image-item'):
-                    bookmark_a = li.find_all('a',class_='bookmark-count _ui-tooltip')
-                    if len(bookmark_a):
-                        num = int(bookmark_a[0].get_text())
-                        if num>=self.number:
-                            lock.acquire()
-                            urls.append('http://www.pixiv.net'+li.a['href'])
-                            lock.release()
+                lis = re.findall('<a href="/bookmark_detail.php\?illust_id=(.*?)" class="bookmark-count _ui-tooltip" data-tooltip=.*?(\w*)</a>', html.text)
+                for li in lis:
+                    if int(li[1]) >=self.number:
+                        lock.acquire()
+                        urls.append('http://www.pixiv.net/member_illust.php?mode=medium&illust_id='+li[0])
+                        lock.release()
             except BaseException,e:
                 print 'page'+str(item)+'出错'
                 lock.acquire()
@@ -49,8 +45,7 @@ s.post('https://www.pixiv.net/login.php',data=payload)
 
 print '标签为 '+keyword+' 收藏数大于等于 '+str(number)+' 的链接'
 html = s.get('http://www.pixiv.net/search.php?word='+keyword)
-soup = BeautifulSoup(html.text,"html.parser")
-pages = (soup.find_all('span',class_='count-badge')[0].get_text())[0:-1]
+pages = re.findall('<span class="count-badge">(\w*).*</span>', html.text)[0]
 pages = int(math.ceil(float(pages)/20))
 print '总页数为 '+str(pages)
 
